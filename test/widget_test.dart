7fd0c771a36845955389d98d6dbad070b2751c59
[raj parihar] <mrrajparihar281@gmail.com>
2026-05-10 00:33:19 +0530

    refactor: clean up widget test by removing unnecessary comments

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,13 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:zenvix/main.dart';
 
 void main() {
-  testWidgets('Zenvix app loads successfully',
-      (WidgetTester tester) async {
-
-    // Build the app
+  testWidgets('Zenvix app loads successfully', (WidgetTester tester) async {
     await tester.pumpWidget(const ZenvixApp());
 
-    // Verify app loaded
     expect(find.byType(ZenvixApp), findsOneWidget);
   });
 }
