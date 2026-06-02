743240574972e7ea4dce08b691e226bcec598762
[raj parihar] <mrrajparihar281@gmail.com>
2026-06-02 17:04:02 +0530

    refactor(pdf_viewer): improve code formatting and readability in PdfViewerScreen and PdfService

--- lib/features/pdf_viewer/services/pdf_service.dart
+++ lib/features/pdf_viewer/services/pdf_service.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
+
 class PdfService {
-  bool fileExists(String path){
+  bool fileExists(String path) {
     final file = File(path);
     return file.existsSync();
-    
   }
-  String getFileName(String path)=> path.split('/').last;
-  Future<File> getPdfFile(String path)async => File(path);
+
+  String getFileName(String path) => path.split('/').last;
+  Future<File> getPdfFile(String path) async => File(path);
 }
