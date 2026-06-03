6ca2582b4d3786d91ad8b37f0a29ec9cc3d3402a
[raj parihar] <mrrajparihar281@gmail.com>
2026-06-03 19:50:54 +0530

    refactor(tool_definition): add ToolCategory enum and extension for improved categorization

--- lib/core/models/tool_definition.dart
+++ lib/core/models/tool_definition.dart
@@ -1,5 +1,35 @@
 import 'package:flutter/material.dart';
 
+enum ToolCategory {
+  pdf,
+  documents,
+  utilities,
+}
+
+extension ToolCategoryX on ToolCategory {
+  String get label {
+    switch (this) {
+      case ToolCategory.pdf:
+        return 'PDF Tools';
+      case ToolCategory.documents:
+        return 'Documents & Sheets';
+      case ToolCategory.utilities:
+        return 'Utilities';
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case ToolCategory.pdf:
+        return Icons.picture_as_pdf_rounded;
+      case ToolCategory.documents:
+        return Icons.description_outlined;
+      case ToolCategory.utilities:
+        return Icons.build_outlined;
+    }
+  }
+}
+
 class ToolDefinition {
   const ToolDefinition({
     required this.id,
@@ -9,22 +39,17 @@ class ToolDefinition {
     required this.accentColor,
     required this.secondaryColor,
     required this.routePath,
+    required this.category,
     this.isAvailable = true,
   });
 
   final String id;
-
   final String title;
-
   final String description;
-
   final IconData icon;
-
   final Color accentColor;
-
   final Color secondaryColor;
-
   final String routePath;
-
+  final ToolCategory category;
   final bool isAvailable;
 }
