dc34a740cea3b94d4d9b9b583a7b3c7391d096f2
[raj parihar] <mrrajparihar281@gmail.com>
2026-06-10 12:29:43 +0530

    fix: move flutter-plugin-loader plugin declaration to pluginManagement block

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -10,6 +10,10 @@ pluginManagement {
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
+    plugins {
+        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    }
+
     repositories {
         google()
         mavenCentral()
@@ -18,7 +22,6 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
