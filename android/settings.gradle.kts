f0449bc618076e1cdab0a709c43a24d08503ad48
[raj parihar] <mrrajparihar281@gmail.com>
2026-06-10 13:12:54 +0530

    fix: move Android and Kotlin plugin declarations to pluginManagement block

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -12,6 +12,8 @@ pluginManagement {
 
     plugins {
         id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+        id("com.android.application") version "8.11.1" apply false
+        id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     }
 
     repositories {
@@ -19,11 +21,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-}
-
-plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
