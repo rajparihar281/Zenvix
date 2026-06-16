1805767c40f0e99246e22a4da71c3e2bf1cb0b1a
[raj parihar] <mrrajparihar281@gmail.com>
2026-06-16 19:23:46 +0530

    feat: Add document associations for native office file support (Android/iOS)

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -10,17 +10,17 @@ pluginManagement {
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
-    plugins {
-        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-        id("com.android.application") version "8.7.3" apply false
-        id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    }
-
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
 }
 
 include(":app")
